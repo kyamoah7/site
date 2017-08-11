0dd3e5ef400f501d071f876ae04a1a7459d77bc5
kyamoah7 <kyamoah7@gmail.com>
2017-08-11 10:17:58 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready( function(){
+  /*scroll navigation*/
+  var headerHeight = $('header').outerHeight();
+  $('.smooth').click(function(e){
+    var linkHref = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop : $(linkHref).offset().top - headerHeight
+    }, 1000);
+    e.preventDefault();
+  })
+  /* carousel for about div*/
+  $('#right-arrow').click(function(){
+    var currentSlide = $('.slide.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.slideToggle(300).addClass('active');
+    if(nextSlide.length == 0){
+      $('.slide').first().slideToggle(300).addClass('active');
+    }
+  });
+  $('#left-arrow').click(function(){
+    var currentSlide = $('.slide.active');
+    var prevSlide = currentSlide.prev();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    prevSlide.slideToggle(300).addClass('.active');
+
+    if(prevSlide.length == 0){
+      $('.slide').last().slideToggle(300).addClass('.active');
+    }
+  });
+});
