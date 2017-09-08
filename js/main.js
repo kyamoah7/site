a1bd46f91860a9a782afccd7f3c3f711bfd233eb
kyamoah7 <kyamoah7@gmail.com>
2017-09-08 14:07:28 -0400

    minus flex added grid layout

--- js/main.js
+++ js/main.js
@@ -8,26 +8,34 @@ $(document).ready( function(){
     }, 1000);
     e.preventDefault();
   })
+
   /* carousel for about div*/
   $('#right-arrow').click(function(){
-    var currentSlide = $('.slide.active');
+    var bio = $('#bio');
+    bio.slideToggle(300);
+    /*var currentSlide = $('.slide1.active');
     var nextSlide = currentSlide.next();
 
     currentSlide.slideToggle(300).removeClass('active');
     nextSlide.slideToggle(300).addClass('active');
     if(nextSlide.length == 0){
       $('.slide').first().slideToggle(300).addClass('active');
-    }
+    }*/
   });
   $('#left-arrow').click(function(){
-    var currentSlide = $('.slide.active');
+    var portfolio = $('#portfolio');
+    portfolio.slideToggle(300);
+    /*var currentSlide = $('.slide2.active');
     var prevSlide = currentSlide.prev();
 
     currentSlide.slideToggle(300).removeClass('active');
-    prevSlide.slideToggle(300).addClass('.active');
+    prevSlide.slideToggle(300).addClass('active');
 
     if(prevSlide.length == 0){
       $('.slide').last().slideToggle(300).addClass('.active');
-    }
+    }*/
   });
+  $('.burger-nav').click(function(){
+      $('nav ul ').toggleClass('open');
+    });
 });
