beb6167bfbcab40f51da8f9912a20ea65acae49c
kyamoah7 <kyamoah7@gmail.com>
2017-10-08 14:10:55 -0400

    new div for projects

--- js/main.js
+++ js/main.js
@@ -10,41 +10,33 @@ $(document).ready( function(){
   });
 
   /* carousel for about div*/
-  var bio = $('#bio');
-  var portfolio = $('#portfolio');
-  var about = $('.aboutword')
+
   $('#right-arrow').click(function(){
 
-    bio.slideToggle(350);
-    portfolio.addClass('deactive');
-    about.toggleClass('out');
-
-    /*var currentSlide = $('.slide1.active');
+    var currentSlide = $('.slide.active');
     var nextSlide = currentSlide.next();
 
     currentSlide.slideToggle(300).removeClass('active');
     nextSlide.slideToggle(300).addClass('active');
     if(nextSlide.length == 0){
       $('.slide').first().slideToggle(300).addClass('active');
-    }*/
+    }
   });
   $('#about').click(function(){
 
   });
   $('#left-arrow').click(function(){
 
-    portfolio.slideToggle(300);
-    portfolio.addClass('deactive');
-    about.toggleClass('out');
-    /*var currentSlide = $('.slide2.active');
+
+    var currentSlide = $('.slide.active');
     var prevSlide = currentSlide.prev();
 
-    currentSlide.slideToggle(300).removeClass('active');
+    currentSlide.slideToggle(300).removeClass('.active');
     prevSlide.slideToggle(300).addClass('active');
 
     if(prevSlide.length == 0){
       $('.slide').last().slideToggle(300).addClass('.active');
-    }*/
+    }
   });
   $('.burger-nav').click(function(){
       $('nav ul ').addClass('open');
